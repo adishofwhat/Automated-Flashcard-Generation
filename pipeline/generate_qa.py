cb3cb94609cb452c9a6c4fefbde9b8a2c0e47804
Sneh Bhandari <60036622+Snehbhandari@users.noreply.github.com>
2024-12-10 16:26:52 -0500

    Add files via upload

---
+++ pipeline/generate_qa.py
@@ -0,0 +1,63 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Initialize the model and tokenizer
+
+# original model path 
+model_path = "potsawee/t5-large-generation-squad-QuestionAnswer"
+
+# fine tuned model path - lora - no such directory exists!! 
+# model_path = "/content/finetuned_squad"
+
+# fine tuned model path - lora + pos 
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_path) 
+
+def gen_qa(context, num_questions=5):
+    """
+    Generate multiple question-answer pairs from the given context.
+    """
+    questions_answers = []
+    context_length = len(context)
+    overlap = 20  # Number of overlapping words
+    chunk_size = (context_length - overlap * (num_questions - 1)) // num_questions
+
+    questions_answers = []
+    for i in range(num_questions):
+        start_idx = i * chunk_size - i * overlap
+        end_idx = start_idx + chunk_size
+        if end_idx > context_length:
+            end_idx = context_length
+
+        chunk = context[start_idx:end_idx]
+
+        # chunk size needs to be <512, return a summary of the chunk within length 512
+        # if len(chunk) > 512:
+            # chunk = summary of chunk
+        
+        # Generate question-answer pair as before
+        inputs = tokenizer(chunk, return_tensors="pt")
+        outputs = model.generate(**inputs, max_length=100, num_return_sequences=1, num_beams=4)
+        question_answer = tokenizer.decode(outputs[0], skip_special_tokens=False)
+        question_answer = question_answer.replace(tokenizer.pad_token, "").replace(tokenizer.eos_token, "")
+        
+        if tokenizer.sep_token in question_answer:
+            question, answer = question_answer.split(tokenizer.sep_token)
+            questions_answers.append((question, answer))
+
+return questions_answers
+
+
+# model_assess file 
+# def generate_qa(context, model, tokenizer):
+#     inputs = tokenizer(context, return_tensors="pt")
+#     outputs = model.generate(**inputs, max_length=100, num_return_sequences=1, num_beams=4) # last 2 are extra, not in the above code- what are they doing 
+#     question_answer = tokenizer.decode(outputs[0], skip_special_tokens=False)
+#     question_answer = question_answer.replace(tokenizer.pad_token, "").replace(tokenizer.eos_token, "")
+#     question, answer = question_answer.split(tokenizer.sep_token)
+
+#     generated_qa = f"{question} {answer}"
+
+#     return generated_qa
+
+
+
